6354e749052c6f3fe261ac7885543c44c54b02bb
bin lu <lu.bin.lu.bin@gmail.com>
2017-11-27 14:52:04 -0500

    xform using R.compose

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const R = require('ramda');
+
 const mapping =
   f =>
     reducing =>
@@ -16,4 +18,27 @@ let foo = mapping(x => x + 1)(
 );
 
 let result = foo([2, 3, 4], 6)
-console.log(result);
+result;
+
+let result1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reduce(
+  mapping(x => x + 1)(filtering(x => x % 2 === 0)((xs, x) => {
+    xs.push(x);
+    return xs;
+  })),
+  []
+);
+
+result1;
+
+const xform = R.compose(
+  mapping(x => x + 1),
+  filtering(x => x % 2 === 0)
+);
+
+let result2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  .reduce(xform((xs, x) => {
+    xs.push(x);
+    return xs;
+  }), []);
+
+result2
